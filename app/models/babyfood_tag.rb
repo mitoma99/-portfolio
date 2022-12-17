0a8c7fe2632c46a2c08366bf612c6762e522b13d
mitoma99 <114859605+mitoma99@users.noreply.github.com>
2022-12-17 11:43:43 +0000

    [update6]baby-care

--- app/models/babyfood_tag.rb
+++
@@ -1,6 +0,0 @@
-class BabyfoodTag < ApplicationRecord
-
-  belongs_to :babyfood
-  belongs_to :tag
-
-end
--- test/models/babyfood_tag_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class BabyfoodTagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
