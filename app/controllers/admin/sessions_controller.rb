4d85b7d4e42071082c0af60f5f5db179c95a3b3c
mitoma99 <114859605+mitoma99@users.noreply.github.com>
2022-12-23 13:25:09 +0000

    [update13]baby-care

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,7 +3,17 @@
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    admin_tweets_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+
 
   # GET /resource/sign_in
   # def new
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -12,3 +12,5 @@ class Tweet < ApplicationRecord
   end
 
 end
+
+
